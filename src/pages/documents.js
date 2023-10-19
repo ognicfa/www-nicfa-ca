24bbf78b75d28274b3f13e03df53bb43cc8e4e0d
Brian Koehler <brian@koehler.ca>
2023-10-19 16:54:18 -0700

    updated collective agreement for 2022-2025

--- src/pages/documents.js
+++ src/pages/documents.js
@@ -6,6 +6,7 @@ import SimpleContent from '@site/src/components/SimpleContent';
 import SimpleHeader from '@site/src/components/SimpleHeader';
 
 import ConstitutionUrl from '@site/static/pdfs/NICFA-Bylaws-Constitution-Code-of-Ethics-2021.pdf';
+import CollectiveAgreementUrl from '@site/static/pdfs/collective-agreement-NIC-NICFA-2022-2025.pdf';
 
 export default function DocumentsPage() {
   const {siteConfig} = useDocusaurusContext();
@@ -15,8 +16,7 @@ export default function DocumentsPage() {
       description="Documents (NICFA)">
       <SimpleHeader bgimage="files">Documents</SimpleHeader>
       <SimpleContent>
-      <p><Link to="https://www.bcbargaining.ca/content/3051/nic-nicfa_april_1_2019_to_march_31_2022.pdf">2019 - 2022 Collective Agreement (Local)</Link></p>
-      <p><Link to="https://www.fpse.ca/sites/default/files/resource-pdfs/2019-2022%20FPSE%20Common%20Agreement.pdf">2019 - 2022 Common Agreement</Link></p>
+      <p><a href={CollectiveAgreementUrl}>2022 - 2025 Collective Agreement</a></p>
       <p><a href={ConstitutionUrl}>Constitution, Bylaws & Code of Ethics</a></p>
       <p><Link to=""></Link></p>
       <p><Link to=""></Link></p>
